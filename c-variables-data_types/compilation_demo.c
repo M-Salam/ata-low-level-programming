096b0d09051dc5d766a00e86c9422c4195da208f
M-Salam <msalamayomide@gmail.com>
2025-09-24 22:35:58 +0100

    Add compilation_steps.txt and compilation_demo.c

---
+++ c-variables-data_types/compilation_demo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// This program prints a message about C compilation.
+
+/*
+ * C is still important today because it is the foundation of many modern languages,
+ * and learning how it compiles helps programmers understand the system-level process.
+ */
+
+int main() {
+    printf("Compilation in C works!\n");
+    return 0;
+}
